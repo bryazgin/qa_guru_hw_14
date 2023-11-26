0eddf8e68917617eab85e521bebae09e5e83fec4
Bryazgin Sergey <sergeybryazgin@gmail.com>
2023-11-26 22:31:42 +0300

    added .env reading

--- qa_guru_hw_14/pages/registration_page.py
+++ qa_guru_hw_14/pages/registration_page.py
@@ -9,10 +9,10 @@ class RegistrationPage():
 
     def open_page(self):
         browser.open('https://demoqa.com/automation-practice-form')
-        browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
-            have.size_greater_than_or_equal(3)
-        )
-        browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
+        # browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
+        #     have.size_greater_than_or_equal(3)
+        # )
+        # browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
 
     def type_first_name(self, first_name):
         browser.element('#firstName').should(be.blank).type(first_name)
